d1be7165a613ff22bdd34a5a51a2dc2c112bf8ee
sawdustofmind <sawdustofmind@gmail.com>
2018-01-21 18:55:33 +0300

    Added Devise users framework
    All configs are default

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :rus_translations, 
     only: [:index, :new, :create, :destroy]
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118080955) do
+ActiveRecord::Schema.define(version: 20180121154321) do
 
   create_table "eng_words", force: :cascade do |t|
     t.string "word"
@@ -34,6 +34,23 @@ ActiveRecord::Schema.define(version: 20180118080955) do
     t.boolean "right"
     t.index ["rus_translation_id"], name: "index_test_line_items_on_rus_translation_id"
     t.index ["vocabulary_test_id"], name: "index_test_line_items_on_vocabulary_test_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vocabulary_tests", force: :cascade do |t|
